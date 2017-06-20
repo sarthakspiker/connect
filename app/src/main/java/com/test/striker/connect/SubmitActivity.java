859bca4f976d101172366f1f02c35cf394bb050a
sarthakspiker <spikerstriker@gmail.com>
2017-06-20 19:02:51 +0530

    remove the underline from editText and beautify the layout a little :p

--- app/src/main/java/com/test/striker/connect/SubmitActivity.java
+++ app/src/main/java/com/test/striker/connect/SubmitActivity.java
@@ -1,8 +1,8 @@
 package com.test.striker.connect;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -22,10 +22,10 @@ public class SubmitActivity extends AppCompatActivity {
         dob = (TextView) findViewById(R.id.dob);
         address = (TextView) findViewById(R.id.address);
 
-        String nameString = getIntent().getStringExtra("name");
-        String emailString = getIntent().getStringExtra("email");
-        String dobString = getIntent().getStringExtra("dob");
-        String addressString = getIntent().getStringExtra("address");
+        String nameString = "Name : " + getIntent().getStringExtra("name");
+        String emailString = "Email : " + getIntent().getStringExtra("email");
+        String dobString = "DOB : " + getIntent().getStringExtra("dob");
+        String addressString = "Address : " + getIntent().getStringExtra("address");
 
         name.setText(nameString);
         email.setText(emailString);
