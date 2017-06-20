a41c2382a170881ea75e90157bf8bb4872414633
sarthakspiker <spikerstriker@gmail.com>
2017-06-20 19:20:39 +0530

    Add AlertDialog to alert user onBackPressed in main activity

--- app/src/main/java/com/test/striker/connect/MainActivity.java
+++ app/src/main/java/com/test/striker/connect/MainActivity.java
@@ -1,8 +1,10 @@
 package com.test.striker.connect;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -24,6 +26,29 @@ TextView textView;
         Intent i = new Intent(MainActivity.this,RegisterActivity.class);
         startActivity(i);
     }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Are you sure you want to exit ?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setTitle("Alert!");
+        alertDialog.show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
