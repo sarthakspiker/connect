85299eff96c2ee73c98091c5f1224acb0bd89232
sarthakspiker <spikerstriker@gmail.com>
2017-06-21 22:02:09 +0530

    Create SplashScreen for the app update the launcher icon

---
+++ app/src/main/java/com/test/striker/connect/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.test.striker.connect;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
